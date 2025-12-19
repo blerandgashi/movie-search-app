66ac1446d48647a72a31c34e369ff73863b86f27
Blerand Gashi <blerandgashi15@gmail.com>
2025-12-20 00:09:53 +0100

    added some js features

--- script.js
+++ script.js
@@ -3,38 +3,56 @@ let movieContainer = document.querySelector("#movie-container")
 const searchBtn = document.querySelector("#search-btn");
 const apiKey = '4276726f8101e56146209a4c21fc8f9d';
 
-searchBtn.addEventListener("click", function(){
+let errorMessage = document.querySelector("#error");
+
+async function getMovie() {
   movieContainer.innerHTML = "";
 
-  fetch (`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${movieInputEl.value}`)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-      data.results.forEach(index => {
-        const posterUrl = `https://image.tmdb.org/t/p/w500${index.poster_path}}`;
+  const response = await fetch (`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${movieInputEl.value}`)
+  const data = await response.json()
+    console.log(data);
+    data.results.forEach(index => {
+      const posterUrl = `https://image.tmdb.org/t/p/w500${index.poster_path}}`;
 
-        if (index.poster_path != null) {
-          movieContainer.innerHTML += `
-            <div class="movie-card">
-              <div class="movie-poster">
-                <img src="${posterUrl}">
+      if (index.poster_path != null) {
+        movieContainer.innerHTML += `
+          <div class="movie-card">
+            <div class="movie-poster">
+              <img src="${posterUrl}">
+            </div>
+            <div class="movie-details">
+              <h2>${index.title}</h2>
+              <div class="movie-info">
+                <span>${index.release_date}</span>
+                <span class="movie-rating">⭐ ${index.vote_average.toFixed(2)}</span>
               </div>
-              <div class="movie-details">
-                <h2>${index.title}</h2>
-                <div class="movie-info">
-                  <span>${index.release_date}</span>
-                  <span class="movie-rating">⭐ ${index.vote_average.toFixed(2)}</span>
-                </div>
-                <p class="movie-plot"></p>
-                <div class="movie-meta">
-                  <p><strong>Director:</strong> ${index.Director}</p>
-                  <p><strong>Actors:</strong> ${index.Actors}</p>
-                  <p><strong>Genre:</strong> ${index.Genre}</p>
-                </div>
+              <p class="movie-plot">
+                ${index.overview}
+              </p>
+              <div class="movie-meta">
+                <p><strong>Popularity:</strong> ${index.popularity}</p>
+                <p><strong>Original language:</strong> ${index.original_language}</p>
+                <p><strong>Vote count:</strong> ${index.vote_count}</p>
               </div>
             </div>
-          ` 
-        }
-      })
+          </div>
+        ` 
+      }
     })
+}
+searchBtn.addEventListener("click", function(){
+  if (movieInputEl.value != "") {
+    getMovie();
+    errorMessage.classList.remove("active");
+  }else{
+    errorMessage.classList.add("active")
+  }
 })
+
+movieInputEl.addEventListener("keypress", function(e){
+  if (e.key === 'Enter') {
+    getMovie();
+  }
+})
+
+movieInputEl.addEventListener("input", getMovie)
